1519859ab8cecf83e2cfd0fc96e14950d91df1c6
David Vidals <david.vidals08@gmail.com>
2022-01-26 01:26:12 -0500

    minor changes

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,19 +1,21 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
+// import axios from "axios";
 
 const API_URL = process.env.REACT_APP_API_URL
 
 function Transactions(){
     const[Transactions,setTransactions]=useState([]);
     useEffect(()=>{
-        axios.get(API_URL + "/Transactions")
-      .then((res)=>{
-        setTransactions(res.data);
-      }).catch((err)=>{
-        throw err;
-      });
-     });
-
+        
+        fetch(API_URL + "/Transactions")
+        .then((res)=>{
+          return res.json();
+        }).then((data)=>{
+          setTransactions(data);
+        }).catch((err)=> {
+          throw err
+        });
+    }, []);
      return(
             <div className="Transactions">
                  {Transactions.map((transaction, amount) => {
