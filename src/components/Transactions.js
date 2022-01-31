d2573d0b000b38d85e8efca3eb0404e76d97d33f
David Vidals <david.vidals08@gmail.com>
2022-01-31 02:15:13 -0500

    minor changes

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -21,7 +21,11 @@ function Transactions(){
 let total = Transactions.reduce((total,current)=> total = Number(total) + Number(current.amount) , 0)
 console.log(total)
 
-
+if (total<1000){
+    <h1 style ='color:red'>{total}</h1>
+} else if (total>1000){
+    <h1 style='color:green'>{total}</h1>
+}
 
  
      return(
